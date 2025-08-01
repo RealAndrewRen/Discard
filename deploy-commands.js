6ebd82c6228b948db3268dcb418ee307097ccb19
Andrew Ren <therealandrewren@gmail.com>
2025-07-31 20:06:31 -0400

    Debugging

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,14 +1,13 @@
-const { SlashCommandBuilder, ContextMenuCommandBuilder } = require('@discordjs/builders');
-const { ApplicationCommandType } = require('discord-api-types/v9');
+require('dotenv').config();
+
 const { REST, Routes } = require('discord.js');
+const { SlashCommandBuilder, ContextMenuCommandBuilder } = require('@discordjs/builders');
+const { ApplicationCommandType } = require('discord-api-types/v10');
 
 const commands = [
-	new SlashCommandBuilder()
+  new SlashCommandBuilder()
     .setName('play')
     .setDescription('Start a game of Discard'),
-  new ContextMenuCommandBuilder()
-    .setName('Make Card')
-    .setType(ApplicationCommandType.Message),
   new SlashCommandBuilder()
     .setName('deck')
     .setDescription('See your Discards on this server'),
@@ -18,11 +17,25 @@ const commands = [
   new SlashCommandBuilder()
     .setName('reload')
     .setDescription('Adds this server to the database if it isn\'t already.'),
-]
-	.map(command => command.toJSON());
+  new ContextMenuCommandBuilder()
+    .setName('Make Card')
+    .setType(ApplicationCommandType.Message)
+].map(command => command.toJSON());
 
+// Replace with your real client and guild ID
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
-rest.put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+(async () => {
+  try {
+    console.log('Refreshing application (guild) commands...');
+
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: commands },
+    );
+
+    console.log('Successfully registered guild commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
